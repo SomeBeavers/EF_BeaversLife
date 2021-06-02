25fc0742feeee9bea380eed28c67526e8e159018
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-06-02 12:24:18 +0300

    Add DeleteMe1.cs

---
+++ EF_BeaversLife/Queries/DeleteMe1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CoreLib_Common;
+
+namespace EF_BeaversLife.Queries
+{
+    public class DeleteMe1
+    {
+        /// <summary>
+        ///     Include is needed.
+        /// </summary>
+        public void UseInverseProperties()
+        {
+            using var context = new AnimalContext();
+            var       persons = context.Persons;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+                foreach (var animalLoved in person.AnimalsLoved)
+                {
+                    Console.Write("\t");
+                    Console.WriteLine(animalLoved);
+                }
+
+                foreach (var animalHated in person.AnimalsHated)
+                {
+                    Console.Write("\t");
+                    Console.Write("\t");
+                    Console.WriteLine(animalHated);
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
