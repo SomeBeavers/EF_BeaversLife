9035cb77be75f1c0c7873ece1efb2f5e1a986d0a
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-10-11 17:11:38 +0300

    RIDER-68510

---
+++ EF_BeaversLife/Queries/DeleteMe3.cs
@@ -0,0 +1,36 @@
+﻿namespace EF_BeaversLife.Queries
+{
+    public class DeleteMe3
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Test()
+        {
+            using var context = new AnimalContext();
+
+            //var queryable = from p in context.Animals
+            //                join t in context.Food
+            //                    on new { p.Food.Id } equals new { t.Id } into gj
+            //                from pt in gj.DefaultIfEmpty()
+            //                select new
+            //                {
+            //                    PhotoUrl = p.Grades.FirstOrDefault(p => p.Age == 1).Id
+            //                };
+
+            var queryable = from p in context.Animals
+                            join t in context.Food
+                                on new { p.Food.Id } equals new { t.Id } into gj
+                            from pt in gj.DefaultIfEmpty()
+                            select new
+                            {
+                                PhotoUrl = p.Grades.FirstOrDefault(p => p.TheGrade == 1).Id
+                            };
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
