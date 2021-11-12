57c1e720253ecc01cdfd2df27aefef65a272eec5
SomeBeavers <lilija146@yandex.ru>
2021-11-12 17:01:01 +0300

    Add snippets for VS.

---
+++ EF_BeaversLife/Queries/UseDBProperty.cs
@@ -0,0 +1,17 @@
+﻿namespace MSSQL_Core_App.Queries
+{
+    internal class UseDBProperty
+    {
+        public void UseEntryState() 
+        {
+            using var context = new AnimalContext();
+            var drawback = context.Drawbacks.First();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            
+            Console.WriteLine(context.Entry(drawback).State);
+            
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
