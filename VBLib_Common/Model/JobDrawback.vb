ff389e17368011b7466816b61e20f02794531aad
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-07-27 18:12:06 +0200

    Remove bad projects.

--- VBLib_Common/Model/JobDrawback.vb
+++
@@ -1,8 +0,0 @@
-﻿Namespace Model
-    Public Class JobDrawback
-        Public Property JobId As Integer
-        Public Property DrawbackId As Integer
-        Public Overridable Property Job As Job
-        Public Overridable Property Drawback As Drawback
-    End Class
-End Namespace
