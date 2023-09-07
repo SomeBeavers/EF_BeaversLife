f7fbc2fd3a32c2841e67a09d8638ed3236faffc5
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-09-07 11:48:21 +0200

    Add CQRS

---
+++ CQRS_Core_App/BeaverCommand.cs
@@ -0,0 +1,12 @@
+﻿using CoreLib_Common;
+using CoreLib_Common.Model;
+
+namespace CQRS_Core_App;
+
+public class BeaverCommand
+{
+    public bool AddBeaver(Beaver beaver)
+    {
+        using var context = new AnimalContext();
+    }
+}
