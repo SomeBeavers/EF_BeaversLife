2dfdae527b8e3ea1f329a7b5200e4869baab1dea
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-09-07 12:13:27 +0200

    UPD

--- CQRS_Core_App/BeaverCommand.cs
+++ CQRS_Core_App/BeaverCommand.cs
@@ -5,8 +5,26 @@ namespace CQRS_Core_App;
 
 public class BeaverCommand
 {
-    public bool AddBeaver(Beaver beaver)
+    public void AddBeaverTest(Beaver beaver)
     {
-        using var context = new AnimalContext();
+        using AnimalContext context = new AnimalContext();
+
+        context.Add(beaver);
+        context.SaveChanges();
+
+        Beaver? beaver1 = new BeaverQuery().GetBeaver(1);
+    }
+}
+
+public class BeaverQuery
+{
+    public Beaver? GetBeaver(int id)
+    {
+        using AnimalContext context = new();
+
+        Beaver? beaver = context.Beavers.FirstOrDefault(beaver => beaver.Id == id);
+
+        new BeaverCommand().AddBeaverTest(beaver ?? new Beaver());
+        return beaver;
     }
 }
