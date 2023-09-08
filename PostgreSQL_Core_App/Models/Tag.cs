dc3c5b2e10a3a9bc95b817eda33422329bee9201
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-09-08 17:49:17 +0200

    Added postgresql project

---
+++ PostgreSQL_Core_App/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostgreSQL_Core_App;
+
+public partial class Tag
+{
+    public int Id { get; set; }
+
+    public string? Text { get; set; }
+
+    public virtual ICollection<CommentTag> CommentTags { get; set; } = new List<CommentTag>();
+}
---
+++ PostgreSQL_Core_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using PostgreSQL_Core_App.Queries;
+
+namespace PostgreSQL_Core_App;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TestCanonicalMethods.DateTimeMethods();
+        Console.WriteLine("Hello, World!");
+    }
+}
