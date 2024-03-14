9b795f8187c990b085cdbf62bcc3064b102a7b11
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2024-03-14 14:35:36 +0100

    Misc

---
+++ MSSQL_CodeFirst_School/Attribute.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MSSQL_CodeFirst_School.Models;
+
+namespace MSSQL_CodeFirst_School
+{
+    public class CustomMaxLengthAttribute : MaxLengthAttribute
+    {
+        public CustomMaxLengthAttribute(int length) : base(length)
+        {
+            ErrorMessage = "Custom error message";
+        }
+    }
+
+    public class UserStringLengthAttribute : StringLengthAttribute
+    {
+        public UserStringLengthAttribute(int maximumLength) : base(maximumLength)
+        {
+        }
+    }
+
+    public class MyDbFunctionAttribute : DbFunctionAttribute
+    {
+        public MyDbFunctionAttribute(string namespaceName, string functionName) : base(namespaceName, functionName)
+        {
+        }
+    }
+
+    public static class PetExtensions
+    {
+        //[MyDbFunction("MyNameSpace", "CustomFilter")]
+        //[DbFunction("MyNameSpace", "CustomFilter")]
+        public static bool CustomFilter(this Pet entityPet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExtensionUsage
+    {
+        public void Test()
+        {
+            var context = new Model1();
+            var res = context.Pets.Where(x => x.CustomFilter()).ToList();
+        }
+    }
+}
--- PostgreSQL_Core_App/Queries/TestCanonicalMethods.cs
+++ PostgreSQL_Core_App/Queries/TestCanonicalMethods.cs
@@ -21,6 +21,7 @@ internal class TestCanonicalMethods
         var dep = context.Departments;
 
         //supported
+        Console.WriteLine(dep.Where(d => d.StartDate.CompareTo(DateTime.Today) > 2).ToList());
         Console.WriteLine(people.Where(a => a.HireDate!.Value.CompareTo(a.EnrollmentDate!.Value) > 2).ToList());
         Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.AddYears(3).Equals(DateTime.Today)).ToList());
         Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.AddDays(3).Equals(DateTime.Today)).ToList());
@@ -33,30 +34,35 @@ internal class TestCanonicalMethods
         Console.WriteLine(people.Where(a => a.HireDate!.Value.Equals(DateTime.Today)).ToList());
         Console.WriteLine(people.Where(p => p.HireDate!.ToString()!.EndsWith("5")).ToList());
         Console.WriteLine(dep.Where(d => DateTime.Compare(d.StartDate, DateTime.Today) > 3).ToList());
+        Console.WriteLine(dep.Where(d => DateTimeOffset.Compare(d.StartDate, DateTimeOffset.Now) > 3).ToList());
+        dep.Where(d => TimeSpan.FromDays(5).Equals(d.StartDate));
+        TimeSpan res;
+        dep.Where(d => TimeSpan.TryParse(d.StartDate.ToString(), out res) == true);
 
         Console.WriteLine(dep.Where(d => d.StartDate.ToString().EndsWith("5")).ToList());
 
-
+        Console.WriteLine(dep.Where(d => Math.Sign(Convert.ToInt32(d.Budget)) == 0));
+        Console.WriteLine(dep.Where(d => Math.Round(Convert.ToDouble(d.Budget)) > 3));
 
         //not supported
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.GetValueOrDefault().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.AddMilliseconds(3).Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.AddTicks(3).Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.HireDate!.Value.IsDaylightSavingTime().Equals(true)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.GetDateTimeFormats().Equals("")).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.GetHashCode().Equals(5)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.GetTypeCode().Equals("")).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToBinary().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToFileTime().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToFileTimeUtc().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToLongDateString().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToLongTimeString().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToShortDateString().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToShortTimeString().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToOADate().Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.Subtract(DateTime.Today).Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.Add(TimeSpan.FromMinutes(5)).Equals(DateTime.Today)).ToList());
-        //Console.WriteLine(dep.Where(d => DateTime.IsLeapYear(d.StartDate.Year) == true).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.GetValueOrDefault().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.AddMilliseconds(3).Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.AddTicks(3).Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.HireDate!.Value.IsDaylightSavingTime().Equals(true)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.GetDateTimeFormats().Equals("")).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.GetHashCode().Equals(5)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.GetTypeCode().Equals("")).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToBinary().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToFileTime().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToFileTimeUtc().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToLongDateString().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToLongTimeString().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToShortDateString().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToShortTimeString().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.ToOADate().Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.Subtract(DateTime.Today).Equals(DateTime.Today)).ToList());
+        Console.WriteLine(people.Where(p => p.EnrollmentDate!.Value.Add(TimeSpan.FromMinutes(5)).Equals(DateTime.Today)).ToList());
+        Console.WriteLine(dep.Where(d => DateTime.IsLeapYear(d.StartDate.Year) == true).ToList());
     }
 
     public static void Method()
